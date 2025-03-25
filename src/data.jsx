445f14acf44012bbbd0e1c4751055b70d303d0dd
mdnadimweb <mdnadim442244@gmail.com>
2025-03-25 14:48:14 +0600

    finalize card making with react

---
+++ src/data.jsx
@@ -0,0 +1,58 @@
+const student = [
+    
+        {
+            name:"Nadim",
+            age:23,
+            job:"student",
+            dec:"he is a good man ",
+        }, 
+    
+        
+    
+        {
+            name:"Jony",
+            age:23,
+            job:"student",
+            dec:"he is a good man ",
+        }, 
+    
+        
+    
+        {
+            name:"Kamrul",
+            age:23,
+            job:"student",
+            dec:"he is a good man ",
+        }, 
+    
+        
+    
+        {
+            name:"Nayem",
+            age:23,
+            job:"student",
+            dec:"he is a good man ",
+        }, 
+    
+        
+    
+        {
+            name:"Masud",
+            age:23,
+            job:"student",
+            dec:"he is a good man ",
+        }, 
+    
+        
+    
+        {
+            name:"Munshi ",
+            age:23,
+            job:"student",
+            dec:"he is a good man ",
+        }, 
+    
+        
+]
+
+export default student ; 
